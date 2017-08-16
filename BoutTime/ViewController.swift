459023818612d9cf2b4ca3ddbc213823645fa8f2
James Mulholland <jmulholland1618@gmail.com>
2017-08-16 16:12:42 +0100

    Add UI outlets

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    // Event buttons
+    @IBOutlet weak var eventButtonOne: UIButton!
+    @IBOutlet weak var eventButtonTwo: UIButton!
+    @IBOutlet weak var eventButtonThree: UIButton!
+    @IBOutlet weak var eventButtonFour: UIButton!
+    // Move buttons
+    @IBOutlet weak var moveDownFullButton: UIButton!
+    @IBOutlet weak var moveUpHalfButtonOne: UIButton!
+    @IBOutlet weak var moveDownHalfButtonOne: UIButton!
+    @IBOutlet weak var moveUpHalfButtonTwo: UIButton!
+    @IBOutlet weak var moveDownHalfButtonTwo: UIButton!
+    @IBOutlet weak var MoveUpFullButton: UIButton!
+    // Next round button
+    @IBOutlet weak var nextRoundButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
